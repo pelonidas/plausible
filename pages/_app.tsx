06d50d3854970a1da655c8d6eb28058d9e60384b
pelonidas <janx803y@easv365.dk>
2022-11-22 22:29:10 +0100

    ss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
       scriptProps={{
         src: "plausible.braveriver-f8c1afc3.northeurope.azurecontainerapps.io",
       }}
-      domain="exur.dk"
+      domain="https://plausible-pi.vercel.app/"
     >
       <Component {...pageProps} />
     </PlausibleProvider>
