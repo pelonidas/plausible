742fe4382c7bd463404b8684a96b2d19a507c39a
pelonidas <janx803y@easv365.dk>
2022-11-22 22:38:38 +0100

    ci: Entities created and modeled into db

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <PlausibleProvider
-      scriptProps={{
-        src: "plausible.braveriver-f8c1afc3.northeurope.azurecontainerapps.io",
-      }}
-      domain="https://plausible-pi.vercel.app/"
-    >
-      <Component {...pageProps} />
-    </PlausibleProvider>
-  );
+  return <Component {...pageProps} />;
 }
