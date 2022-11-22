bc26d0aa49c9d11eb0bf36d025842424e3253466
pelonidas <janx803y@easv365.dk>
2022-11-22 22:26:18 +0100

    s

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider
+      scriptProps={{
+        src: "plausible.braveriver-f8c1afc3.northeurope.azurecontainerapps.io",
+      }}
+      domain="exur.dk"
+    >
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
